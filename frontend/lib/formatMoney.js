a83fdbe002070e6060da53a9f5a2783563f9125e
Andrés Felipe Álvarez Caballero <andres_9722@outlook.com>
2020-10-13 22:42:32 -0500

    Add project

--- frontend/lib/formatMoney.js
+++
@@ -1,9 +0,0 @@
-export default function (amount) {
-  const options = {
-    style: "currency",
-    currency: "COP",
-  };
-
-  const formatter = new Intl.NumberFormat("es-CO", options);
-  return formatter.format(amount);
-}
