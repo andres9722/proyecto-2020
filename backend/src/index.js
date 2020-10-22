2d5e697c1e96e80225e3a03187570f2ae69c5ccb
Andrés Felipe Álvarez Caballero <andres_9722@outlook.com>
2020-10-22 18:50:18 -0500

    Added app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
@@ -27,12 +27,13 @@ server.express.use(async (req, res, next) => {
   if (!req.userId) return next();
   const user = await db.query.user(
     { where: { id: req.userId } },
-    '{ id, permissions, email, name }'
+    "{ id, permissions, email, name }"
   );
   req.user = user;
   next();
 });
 
+//startttt
 server.start(
   {
     cors: {
@@ -40,7 +41,7 @@ server.start(
       origin: process.env.FRONTEND_URL,
     },
   },
-  deets => {
+  (deets) => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
