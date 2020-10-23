60328cebad351402e45af5ed49d3fd92168be0f2
Andrés Felipe Álvarez Caballero <andres_9722@outlook.com>
2020-10-22 19:05:33 -0500

    Added app front

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
 export const prodEndpoint =
-  "https://us1.prisma.sh/andresfelipe9722/tiendapp/dev";
+  "https://tiendapp-production-35cd9a2a36.herokuapp.com/tiendapp-prod/prod";
 export const perPage = 4;
